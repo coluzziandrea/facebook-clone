40ecd6728c08083f733eec762468188ece59f4dc
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2022-02-28 17:41:53 +0100

    header left

--- facebook-clone/components/Header.js
+++ facebook-clone/components/Header.js
@@ -1,17 +1,38 @@
 import Image from 'next/image';
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
-
       {/* left */}
-      <div>
+      <div className='flex items-center'>
         <Image
           src='https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png'
           width={40}
           height={40}
           layout='fixed'
         />
+        <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
+          <SearchIcon className='h-6 text-gray-600' />
+          <input
+            className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500'
+            type='text'
+            placeholder='Search on Facebook'
+          />
+        </div>
       </div>
 
       {/* center */}
--- facebook-clone/next.config.js
+++ facebook-clone/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn0.iconfinder.com'],
+    domains: [
+      'cdn0.iconfinder.com',
+      'platform-lookaside.fbsbx.com',
+      'firebasestorage.googleapis.com',
+    ],
   },
 };
 
