36edaea0d3735df91e1c26dcc7c9ae93376e314a
Andrea Coluzzi <coluzziandrea.dev@gmail.com>
2022-08-20 13:41:37 +0200

    upload photo

--- facebook-clone/components/InputBox.js
+++ facebook-clone/components/InputBox.js
@@ -1,30 +1,67 @@
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import { EmojiHappyIcon } from '@heroicons/react/outline';
-import { CameraIcon, VideoCameraIcon } from '@heroicons/react/solid';
-import { useRef } from 'react';
-import { db } from '../firebase';
-import { serverTimestamp, collection, addDoc } from 'firebase/firestore';
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { EmojiHappyIcon } from "@heroicons/react/outline";
+import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
+import { useRef, useState } from "react";
+import { db, storage } from "../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { serverTimestamp, collection, addDoc, setDoc, doc } from "firebase/firestore";
 
 function InputBox() {
   const { data: session } = useSession();
 
   // reference to input field
   const inputRef = useRef(null);
+  const filePickerRef = useRef(null);
+
+  const [imageToPost, setImageToPost] = useState(null);
 
   const sendPost = (e) => {
     e.preventDefault();
 
     if (!inputRef.current.value) return;
-    addDoc(collection(db, 'posts'), {
+    addDoc(collection(db, "posts"), {
       message: inputRef.current.value,
       name: session.user.name,
       email: session.user.email,
       image: session.user.image,
       timestamp: serverTimestamp(),
+    }).then((document) => {
+      if (imageToPost) {
+        const uploadTask = uploadBytes(ref(storage, `posts/${document.id}`), imageToPost);
+
+        removeImage();
+
+        uploadTask.then((snapshot) => {
+          // upload completes, add the URL to the post
+          getDownloadURL(ref(storage, `posts/${document.id}`)).then((url) => {
+            setDoc(
+              doc(db, "posts", document.id),
+              {
+                postImage: url,
+              },
+              { merge: true }
+            );
+          });
+        });
+      }
     });
 
-    inputRef.current.value = '';
+    inputRef.current.value = "";
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.onload = (readerEvent) => {
+        setImageToPost(readerEvent.target.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
   };
 
   return (
@@ -48,6 +85,16 @@ function InputBox() {
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className='flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer'
+          >
+            <img className='h-10 object-contain' src={imageToPost} alt='image to post' />
+            <p className='text-xs text-red-500 text-center'>Remove</p>
+          </div>
+        )}
       </div>
 
       <div className='flex justify-evenly p-3 border-t'>
@@ -55,9 +102,10 @@ function InputBox() {
           <VideoCameraIcon className='h-7 text-red-500' />
           <p className='text-xs sm:text-sm xl:text-base'>Live Video</p>
         </div>
-        <div className='inputIcon'>
+        <div onClick={() => filePickerRef.current.click()} className='inputIcon'>
           <CameraIcon className='h-7 text-green-400' />
           <p className='text-xs sm:text-sm xl:text-base'>Photo/Video</p>
+          <input ref={filePickerRef} onChange={addImageToPost} type='file' hidden></input>
         </div>
         <div className='inputIcon'>
           <EmojiHappyIcon className='h-7 text-yellow-300' />
--- facebook-clone/firebase.js
+++ facebook-clone/firebase.js
@@ -1,17 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBA5DqL7QDLsTfIpAie4EjKmgEywTN_RjM',
-  authDomain: 'facebook-coluzzi.firebaseapp.com',
-  projectId: 'facebook-coluzzi',
-  storageBucket: 'facebook-coluzzi.appspot.com',
-  messagingSenderId: '336157107667',
-  appId: '1:336157107667:web:af428b6c0cd6d92690bb26',
+  apiKey: "AIzaSyBA5DqL7QDLsTfIpAie4EjKmgEywTN_RjM",
+  authDomain: "facebook-coluzzi.firebaseapp.com",
+  projectId: "facebook-coluzzi",
+  storageBucket: "facebook-coluzzi.appspot.com",
+  messagingSenderId: "336157107667",
+  appId: "1:336157107667:web:af428b6c0cd6d92690bb26",
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
+const storage = getStorage(app);
 
-export { db };
+export { db, storage };
